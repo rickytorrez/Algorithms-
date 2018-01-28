82b145e36ed35c7b7c723c126ba6f885a7b48081
Ricardo Torrez <torrez.ricky@ymail.com>
2018-01-27 19:29:04 -0500

    Add files via upload

---
+++ countdownByFours.js
@@ -0,0 +1,12 @@
+// Count down by increments of four
+// JavaScript
+
+function countdownByFours(){
+    var num = 1000;
+    while (num > 0){
+        console.log(num);
+        num -=4;
+    }
+}
+
+countdownByFours()
