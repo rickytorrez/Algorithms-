7b3a30b773e000b624f2ec73cedf337e26203570
Ricardo Torrez <torrez.ricky@ymail.com>
2018-01-29 23:15:55 -0500

    Add files via upload

---
+++ sumToOneDigit.js
@@ -0,0 +1,19 @@
+// Sum to One Digit
+// JavaScript
+
+function sumToOneDigit(num){
+
+    if (num < 10){
+        return num;
+    }
+    var str = num.toString();
+    var sum = 0;
+    for (var i = 0; i < str.length; i++){
+        sum += Number(str[i]);
+    }
+
+    return sumToOneDigit(sum);
+
+}
+
+console.log(sumToOneDigit(99))
